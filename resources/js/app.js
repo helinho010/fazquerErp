061878d3f5e31b43808607da6c1685ad92b0505c
Eddy Claros <eddyclarosc@gmail.com>
2022-04-06 15:47:27 -0400

    funciona laravel 8 + vue 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+import { createApp } from 'vue';
+
+createApp ({})
+    .component('example',require('./components/Example.vue').default)
+    .component('another-example',require('./components/AnotherExample.vue').default)
+    .mount('#app');
+
+
+
